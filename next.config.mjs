149efc321dbc984f63068b915bbb075bc5484dac
ethan-rng <ethan.rong@gmail.com>
2025-03-30 04:52:58 -0400

    restesting ci/cd pipepline

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
-/** @type {import('next').NextConfig} */  
-import { withNetlify } from '@netlify/next';
+import netlifyNext from '@netlify/next';
+const { withNetlify } = netlifyNext;
 
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
     domains: ['hebbkx1anhila5yf.public.blob.vercel-storage.com'],
